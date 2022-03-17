00e71621cf8661fbfe47de39699a4069e41cb55d
Dmitriy Artemyev <dmitrie098@gmail.com>
2022-03-18 01:54:43 +0300

    quiz

---
+++ app.js
@@ -0,0 +1,119 @@
+document.querySelector(".container1-img1").addEventListener('click', () => {
+    document.querySelector(".container1").hidden = true
+    document.querySelector(".container2").hidden = false
+})
+
+document.querySelector(".container__block-image2-1").addEventListener("click",() => {
+    document.querySelector(".container1-1-2").hidden = true
+    document.querySelector(".container2").hidden = false
+})
+
+document.querySelector(".container__arrow1-1-2").addEventListener("click",() => {
+    document.querySelector(".container1-1-2").hidden = true
+    document.querySelector(".container1").hidden = false
+})
+
+document.querySelector(".container__block-frame3").addEventListener("click",() => {
+    document.querySelector(".container1-1-2").hidden = true
+    document.querySelector(".container1-1-2-1").hidden = false
+})
+
+document.querySelector(".container__arrow1-1-2-1").addEventListener("click",() => {
+    document.querySelector(".container1-1-2-1").hidden = true
+    document.querySelector(".container1").hidden = false
+})
+
+document.querySelector(".container__block-image2-2").addEventListener("click",() => {
+    document.querySelector(".container2").hidden = true
+    document.querySelector(".container1-1-2").hidden = false
+})
+
+document.querySelector(".container__block-frame4").addEventListener("click",() => {
+    document.querySelector(".container1-1-2").hidden = true
+    document.querySelector(".container1-1-2-2").hidden = false
+})
+
+document.querySelector(".container__arrow1-1-2-2").addEventListener("click",() => {
+    document.querySelector(".container1-1-2-2").hidden = true
+    document.querySelector(".container1").hidden = false
+})
+
+document.querySelector(".container1-img2").addEventListener('click', () => {
+    document.querySelector(".container1").hidden = true
+    document.querySelector(".container2_").hidden = false
+})
+
+document.querySelector(".container__block-image2-3").addEventListener('click', () => {
+    document.querySelector(".container2_").hidden = true
+    document.querySelector(".container2-1").hidden = false
+})
+
+document.querySelector(".container__arrow_").addEventListener("click",() => {
+    document.querySelector(".container2_").hidden = true
+    document.querySelector(".container1").hidden = false
+})
+
+document.querySelector(".container__block-image2-4").addEventListener('click', () => {
+    document.querySelector(".container2_").hidden = true
+    document.querySelector(".container2-2").hidden = false
+})
+
+document.querySelector(".container__arrow2-2").addEventListener("click",() => {
+    document.querySelector(".container2-2").hidden = true
+    document.querySelector(".container1").hidden = false
+})
+
+document.querySelector(".container__block-frame8").addEventListener('click', () => {
+    document.querySelector(".container2-1").hidden = true
+    document.querySelector(".container2-1-2").hidden = false
+})
+
+document.querySelector(".container__arrow2-1-2").addEventListener("click",() => {
+    document.querySelector(".container2-1-2").hidden = true
+    document.querySelector(".container1").hidden = false
+})
+
+document.querySelector(".container__block-frame7").addEventListener('click', () => {
+    document.querySelector(".container2-1").hidden = true
+    document.querySelector(".container2-1-1").hidden = false
+})
+
+document.querySelector(".container__arrow2-1-1").addEventListener("click",() => {
+    document.querySelector(".container2-1-1").hidden = true
+    document.querySelector(".container1").hidden = false
+})
+
+document.querySelector(".container__arrow").addEventListener("click",() => {
+    document.querySelector(".container2").hidden = true
+    document.querySelector(".container1").hidden = false
+})
+
+document.querySelector(".container__block-image2-1").addEventListener("click",() => {
+    document.querySelector(".container2").hidden = true
+    document.querySelector(".container1-1-1").hidden = false
+})
+
+document.querySelector(".container__arrow1-1-1").addEventListener("click",() => {
+    document.querySelector(".container1-1-1").hidden = true
+    document.querySelector(".container2").hidden = false
+})
+
+document.querySelector(".container__block-frame1").addEventListener("click",() => {
+    document.querySelector(".container1-1-1").hidden = true
+    document.querySelector(".container1-1-1-1").hidden = false
+})
+
+document.querySelector(".container__arrow1-1-1-1").addEventListener("click",() => {
+    document.querySelector(".container1-1-1-1").hidden = true
+    document.querySelector(".container1").hidden = false
+})
+
+document.querySelector(".container__block-frame2").addEventListener("click",() => {
+    document.querySelector(".container1-1-1").hidden = true
+    document.querySelector(".container1-1-1-2").hidden = false
+})
+
+document.querySelector(".container__arrow1-1-1-2").addEventListener("click",() => {
+    document.querySelector(".container1-1-1-2").hidden = true
+    document.querySelector(".container1").hidden = false
+})
